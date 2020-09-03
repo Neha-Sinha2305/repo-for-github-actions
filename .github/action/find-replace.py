eb050092a40330f824a42c4f9348c1806f9f026e
k10145 <Neha.Sinha@skatteetaten.no>
2020-09-03 12:33:47 +0200

    using python file

--- .github/action/find-replace.py
+++ .github/action/find-replace.py
@@ -1,6 +1,7 @@
 import os
 
 filepath= os.path.join(str(os.environ.get("GITHUB_WORKSPACE")),str(os.environ.get("FILE_NAME")))
+
 with open(filepath) as f:
     newText=f.read().replace(os.environ.get("find"),os.environ.get("replace"))
 
