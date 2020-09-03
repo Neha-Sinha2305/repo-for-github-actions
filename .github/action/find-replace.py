44498dcafb2d55a5c506bae551d54bee5e0ac326
k10145 <Neha.Sinha@skatteetaten.no>
2020-09-03 14:16:17 +0200

    using python file

--- .github/action/find-replace.py
+++ .github/action/find-replace.py
@@ -1,9 +1,9 @@
 import os
 
-filepath= os.path.join(str(os.environ.get("GITHUB_WORKSPACE")),str(os.environ.get("FILE_NAME")))
+filepath = os.path.join(str(os.environ.get("GITHUB_WORKSPACE")), str(os.environ.get("FILE_NAME")))
 
 with open(filepath) as f:
-    newText=f.read().replace(os.environ.get("find"),os.environ.get("replace"))
+    newText = f.read().replace(os.environ.get("find"), os.environ.get("replace"))
 
 with open(filepath, "w") as f:
     f.write(newText)
