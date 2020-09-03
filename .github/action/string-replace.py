bfc3fed341fbb226c2e9fea9492a76865daf4155
k10145 <Neha.Sinha@skatteetaten.no>
2020-09-03 11:46:17 +0200

    using python file

--- .github/action/string-replace.py
+++ .github/action/string-replace.py
@@ -1,10 +1,11 @@
 import os
 
 filepath= str(os.environ.get("GITHUB_WORKSPACE")) + "/" + str(os.environ.get("FILE_NAME"))
-with open(filepath, "rt") as fin:
-    with open(filepath, "wt") as fout:
-        for line in fin:
-            fout.write(line.replace(find, replace))
+with open(filepath) as f:
+    newText=f.read().replace(os.environ.get("find"),os.environ.get("replace"))
+
+with open(filepath, "w") as f:
+    f.write(newText)
 
 with open(filepath, "r") as text_file:
     print(text_file.read())
